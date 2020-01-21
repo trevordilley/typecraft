a9541ab1c5b4acd2b020688c765cb5d7f5d30f6f
trevordilley <trevor@twentyideas.com>
2020-01-20 22:41:51 -0800

    Centered main typing text, supplied a nice animation for now

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,35 @@ import React from "react"
 import * as Phaser from "phaser"
 import {TypingScene} from "./typingScene/TypingScene"
 
+export const THEME = {
+    stageWidth: 800,
+    stageHeight: 600,
+    // Really important we use a Mono space font so I can calculate the widths
+    // based on the characters of the text.
+    typingFont: 'Roboto Mono',
+    typingFontSize: 40,
+    // PIXEL WIDTH IS DIRECTLY TIED TO
+    // THE typingFontSize AND NEEDS TO BE RECALIBRATED
+    // IF WE CHANGE IT
+    typingPixelWidth: 24
+}
+
 export const BASE_CONFIG: Phaser.Types.Core.GameConfig = {
     title: "TypeCraft",
-    width: 800,
-    height: 600,
+    width: THEME.stageWidth,
+    height: THEME.stageHeight,
     type: Phaser.AUTO,
     backgroundColor: '#ffffff'
 }
 
 const App = () => {
+
     return (
-        <TypingScene/>
+        <span
+            id={"container"}
+            style={{fontFamily: THEME.typingFont}}>
+            <TypingScene/>
+        </span>
     )
 }
 
--- src/typingScene/activeWord/ActiveWord.tsx
+++ src/typingScene/activeWord/ActiveWord.tsx
@@ -1,25 +1,54 @@
-import React from 'react'
+import React from "react"
+import {THEME} from "../../App"
 
-const chars = (word: string) => word.split('').map(c => <span>{c}</span>)
-
-const Word: React.FC<{word: string}> = ({word}) => {
-    return (<>{chars(word)}</>)
+const chars = (id: string, word: string, isCurrent?: boolean) => {
+    const lastChar = word.length - 1
+    const lastCharClass = `puff-in-bl`
+    return word
+        .split("")
+        .map((c, idx) =>
+            <span
+                className={(idx === lastChar && isCurrent) ? lastCharClass : ""}
+                key={`${idx}-${id}-${c}`}
+            >
+                {c}
+            </span>
+        )
 }
+const Word: React.FC<{ id: string, word: string, isCurrent?: boolean }> =
+    ({id, word, isCurrent}) => {
+        return (<>{chars(id, word, isCurrent)}</>)
+    }
 
 
-export const ActiveWord: React.FC <{targetWord: string, currentWord: string}> =
+export const ActiveWord: React.FC<{ targetWord: string, currentWord: string }> =
     ({targetWord, currentWord}) => {
+        // Needs to be one more pixel than the width of the characters to fit
+        // on one line
+        const wordPixelWidth = targetWord.length * THEME.typingPixelWidth + 1
 
-    // TODO: Don't use fixed height
-    return (
-        <div style={{height: 30}}>
-            <div style={{position: 'absolute',  color: '#ddd'}}>
-                <Word word={targetWord}/>
+        // If we want to move the typing text elsewhere we're going to have to use
+        // the grid CSS rule instead of flex
+        return (
+            <div style={{
+                height: THEME.stageHeight,
+                width: THEME.stageWidth,
+                pointerEvents: "none",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                fontSize: THEME.typingFontSize
+            }}
+            >
+                <div style={{display: "flex", justifyContent: "center"}}>
+                    <div style={{position: "absolute", color: "#ddd", width: wordPixelWidth}}>
+                        <Word id={"target"} word={targetWord}/>
+                    </div>
+                    <div style={{position: "absolute", width: wordPixelWidth}}>
+                        <Word id={"current"} word={currentWord} isCurrent={true}/>
+                    </div>
+                </div>
             </div>
-            <div style={{position: 'absolute'}}>
-                <Word word={currentWord}/>
-            </div>
-        </div>
-    )
+        )
 
-}
+    }
